47b6410ce9ad5fec3f88cc46a59d249e522d008c
shanisfairy <shanisfairy@163.com>
2023-10-25 21:54:16 +0800

    web框架2.0

---
+++ web/src/components/commonTag.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="commonTag">
+    weiba
+  </div>
+</template>
+
+<script>
+export default {
+  name: "commonTag",
+  data() {
+    return {};
+  },
+  methods:{
+   
+  },
+  computed: {
+    
+  },
+};
+</script>
+
+<style lang="less" scoped>
+.commonTag{
+  width: 90%;
+  z-index: 10;
+  height: 15%;
+  padding: 20px;
+  top: 82%;
+  position: absolute;
+  background-color: rgb(103, 123, 123);
+}
+</style>
