8df64c9736cdd5ade042e774a186a337b1e62949
Wcilzo <911402070@qq.com>
2023-11-02 20:20:32 +0800

    feat:bug

--- qiniu-server/qiniu-security/src/test/java/com/video/TestApplication.java
+++ qiniu-server/qiniu-security/src/test/java/com/video/TestApplication.java
@@ -2,23 +2,19 @@ package com.video;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import javax.sql.DataSource;
 
 @SpringBootTest
+@SpringBootConfiguration
 public class TestApplication {
-    @Autowired
-    private DataSource dataSource;
     @Test
     public void TestBCryptPasswordEncoder() {
         BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
         String encode = passwordEncoder.encode("abc");
         System.out.println(encode);
-    }
-    @Test
-    public void testConnection() throws Exception {
-        System.out.println(dataSource.getConnection());
     }
 }
