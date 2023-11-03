ed0c4bd50051474a16c7ae8d8f32b9ccbc552bf4
Wcilzo <911402070@qq.com>
2023-11-03 15:04:38 +0800

    feat:bug

--- qiniu-server/qiniu-security/src/main/java/com/security/service/LoginService.java
+++ qiniu-server/qiniu-security/src/main/java/com/security/service/LoginService.java
@@ -2,7 +2,9 @@ package com.security.service;
 
 import com.common.model.ResponseResult;
 import com.security.pojo.User;
+import org.springframework.stereotype.Service;
 
+@Service
 public interface LoginService {
     public ResponseResult login(User user);
     public Object getLoginUser(String userId);
