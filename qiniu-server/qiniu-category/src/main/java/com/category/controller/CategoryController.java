4bc25125461bf62f75d672dbabdc8df69e2c724e
Wcilzo <911402070@qq.com>
2023-11-06 01:02:18 +0800

    feat:test

--- qiniu-server/qiniu-category/src/main/java/com/category/controller/CategoryController.java
+++ qiniu-server/qiniu-category/src/main/java/com/category/controller/CategoryController.java
@@ -3,7 +3,7 @@ package com.category.controller;
 import com.category.service.CategoryService;
 import com.common.model.ResponseResult;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,12 +12,12 @@ public class CategoryController {
     @Autowired
     private CategoryService categoryService;
 
-    @RequestMapping("/home/getCategoryHomeList")
+    @GetMapping("/home/getCategoryHomeList")
     public ResponseResult getCategoryHomeList() {
         return categoryService.getCategoryHomeList();
     }
 
-    @RequestMapping("/home/getCategoryTagList")
+    @GetMapping("/home/getCategoryTagList")
     public ResponseResult getCategoryTagList(Integer id) {
         return categoryService.getCategoryTagList(id);
     }
