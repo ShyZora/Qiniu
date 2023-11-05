ff5f0c6a07ace5b260d3b198bb3ae52022045f52
Wcilzo <911402070@qq.com>
2023-11-06 00:38:25 +0800

    feat:fix bug

--- qiniu-server/qiniu-category/src/main/java/com/category/dao/VideoMapper.java
+++ qiniu-server/qiniu-category/src/main/java/com/category/dao/VideoMapper.java
@@ -2,6 +2,8 @@ package com.category.dao;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.category.pojo.Video;
+import org.apache.ibatis.annotations.Mapper;
 
+@Mapper
 public interface VideoMapper extends BaseMapper<Video> {
 }
