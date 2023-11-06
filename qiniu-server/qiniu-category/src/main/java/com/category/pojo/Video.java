2dbc841ebe58791594b36f2bb192e789b027ab01
Wcilzo <911402070@qq.com>
2023-11-06 10:35:58 +0800

    feat:test

--- qiniu-server/qiniu-category/src/main/java/com/category/pojo/Video.java
+++ qiniu-server/qiniu-category/src/main/java/com/category/pojo/Video.java
@@ -11,7 +11,7 @@ import java.time.LocalDateTime;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@TableName(value = "Video")
+@TableName(value = "video")
 public class Video {
     private static final long serialVersionUID = 1L;
     @TableId
