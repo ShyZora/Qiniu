f5f378ca0ef7f72a3ed2905d6cdce0157f622266
Wcilzo <911402070@qq.com>
2023-11-06 09:42:19 +0800

    feat:add security on category, now test

---
+++ qiniu-server/qiniu-category/src/main/java/com/category/config/WebConfig.java
@@ -0,0 +1,28 @@
+package com.category.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                //关闭csrf
+                .csrf().disable()
+                //不通过Session获取SecurityContext
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                // 对于登录接口 允许匿名访问
+                .antMatchers("/home/getCategoryHomeList").anonymous()
+                .antMatchers("/home/getCategoryTagList/**").permitAll()
+                // 除上面外的所有请求全部需要鉴权认证
+                .anyRequest().authenticated();
+    }
+}
--- qiniu-server/qiniu-security/src/main/java/com/security/config/WebConfig.java
+++ qiniu-server/qiniu-security/src/main/java/com/security/config/WebConfig.java
@@ -45,8 +45,6 @@ public class WebConfig extends WebSecurityConfigurerAdapter {
                 // 对于登录接口 允许匿名访问
                 .antMatchers("/user/login").anonymous()
                 .antMatchers("/user/register").anonymous()
-                .antMatchers("/home/getCategoryHomeList").anonymous()
-                .antMatchers("/home/getCategoryTagList/**").permitAll()
                 // 除上面外的所有请求全部需要鉴权认证
                 .anyRequest().authenticated();
         // 将 token 检验层放在过滤器链前面
